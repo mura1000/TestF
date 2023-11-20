beed150f2661501cbfd35e3c6682fea014dbbeec
mura1000 <149577457+mura1000@users.noreply.github.com>
2023-11-20 17:46:17 +0900

    Initial Commit

---
+++ TestF/TestFApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFApp.swift
+//  TestF
+//
+//  Created by mura on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct TestFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
